c9ebef14351a6923a9c40d4d9f54d157827fe215
OnlyDragon0403 <mokhnoalina@gmail.com>
2022-10-25 01:43:08 +0900

    "add:FError_model"

---
+++ Source/Algorand/Public/Models/FError.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "FError.generated.h"
+
+USTRUCT(BlueprintType)
+struct FError {
+    GENERATED_USTRUCT_BODY()
+
+    UPROPERTY(BlueprintReadOnly, Category = "Default")
+    FString message;
+
+    FError() {}
+
+    FError(const FString& errorMessage) : message(errorMessage) {}
+}
