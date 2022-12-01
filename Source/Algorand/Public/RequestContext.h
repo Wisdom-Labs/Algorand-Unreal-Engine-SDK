036ba80c9e7dc5aa50a284746205f46155d5e3bc
OnlyDragon0403 <mokhnoalina@gmail.com>
2022-12-02 02:24:51 +0900

    "add:comments_onto_request_context_manager"

--- Source/Algorand/Public/RequestContext.h
+++ Source/Algorand/Public/RequestContext.h
@@ -1,10 +1,23 @@
-#pragma once
+// Copyright 2022, Wisdom Labs. All Rights Reserved
 
+#pragma once
 #include "BaseModel.h"
 #include "CoreMinimal.h"
 #include "Misc/TVariant.h"
 #include "UnrealApi.h"
 
+/**
+ * ToDo:
+ * for asset bases on transactions and wallet provider
+ * add like txs
+ * Key Registration, Asset Configuration, Freeze and Transafer, State Proof
+ */
+
+/**
+ * declare ApiDelegate and Request to build request context
+ */
+
+/// declaring templates of Api Delegate as TVariant
 using ApiDelegate = TVariant<
 	algorand::api::UnrealApi::FAlgorandGenerateWalletGetDelegate,
 	algorand::api::UnrealApi::FAlgorandGetaddressbalanceGetDelegate,
@@ -12,7 +25,12 @@ using ApiDelegate = TVariant<
 	algorand::api::UnrealApi::FAlgorandApplicationCallTransactionGetDelegate
 >;
 
+/// Request Contxet info
 struct RequestContext {
+
+	/// request pointer 
 	TSharedPtr<algorand::vertices::Request>  request;
+ 
+	///delegate callback
 	TSharedPtr<ApiDelegate> delegate;
 };
