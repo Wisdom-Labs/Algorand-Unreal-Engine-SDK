6366ea1ae8f496e868ba994a297f60cbff2abd2f
Bruno <Bruno@DESKTOP-V0A4M0I>
2023-03-05 19:01:54 +0100

    "add::new_BP_function_for_comma_digits"

--- Source/Algorand/Private/UAlgorandBPFunctionLibrary.cpp
+++ Source/Algorand/Private/UAlgorandBPFunctionLibrary.cpp
@@ -1,7 +1,7 @@
 #include "UAlgorandBPFunctionLibrary.h"
 
 /**
- * @brief print value as string after convert to long long int
+ * @brief print value as string after convert long long int
  */
 FString
 UAlgorandBPFunctionLibrary::Conv_FUInt64ToString(const FUInt64& value)
@@ -16,4 +16,26 @@ FUInt64
 UAlgorandBPFunctionLibrary::Conv_FStringToUInt64(const FString& value)
 {
     return FUInt64(FCString::Strtoui64(*value, NULL, 10));
+}
+
+/**
+ * @brief print value as comma string after convert long long int
+ */
+FString
+UAlgorandBPFunctionLibrary::Conv_FUInt64ToCommaString(const FUInt64& value, const FUInt64& count = 3)
+{
+	FString CommaString = FString::Printf(TEXT("%llu"), value.Value);;
+
+	// Insert comma every count digits
+	int32 InsertPosition = CommaString.Len() - count; // Insert position for first comma
+
+	while (InsertPosition > 0)
+	{
+		CommaString.InsertAt(InsertPosition, TEXT(","));
+		InsertPosition -= count;
+	}
+	
+	UE_LOG(LogTemp, Warning, TEXT("Comma String: %s"), *CommaString);
+	
+	return CommaString;
 }
