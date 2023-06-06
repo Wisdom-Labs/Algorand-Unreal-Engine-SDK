e99f7f2ab95576704d5fdf95a7d579df6ee81dd1
onlydragon0403 <brunogendron6827@gmail.com>
2023-06-05 18:19:34 -0700

    "add:struct_of_Arc03&Arc69"

---
+++ Source/AlgorandAPI/Private/Arc/Arc03.cpp
@@ -0,0 +1,74 @@
+//
+// Created by Bruno on 6/5/2023.
+//
+
+#include "Arc/Arc03.h"
+
+const std::string BASE_URL_OF_IPFS = "https://ipfs.io/ipfs/";
+
+Arc03::Arc03(uint64_t asset_id) {
+    this->from_asset(asset_id);
+    this->from_tx(asset_id);
+}
+
+bool Arc03::IsVerify() {
+    std::string suffix("#arc3");
+    if (suffix.size() > asset.params.url.Len()) return false;
+    return asset.params.url.EndsWith(suffix.c_str(), ESearchCase::IgnoreCase);
+}
+
+void Arc03::from_asset_url(std::string &url) {
+    RestClient restClient(BASE_URL_OF_IPFS, "");
+
+    std::string asset_url = url;
+    std::string prefix("ipfs://");
+    if(asset_url.compare(0, prefix.size(), prefix) == 0) {
+        asset_url.erase(0, prefix.size());
+    }
+
+    auto resp = restClient.get(asset_url);
+
+    try{
+
+        for (auto itr : resp.json->Values) {
+            if (std::strcmp(TCHAR_TO_UTF8(*itr.Key) ,"standard") == 0)
+                metadata.standard = itr.Value->AsString();
+            if (std::strcmp(TCHAR_TO_UTF8(*itr.Key) ,"name") == 0)
+                metadata.name = itr.Value->AsString();
+
+            if (std::strcmp(TCHAR_TO_UTF8(*itr.Key) ,"decimals") == 0)
+                metadata.decimals = itr.Value->AsNumber();
+
+            if (std::strcmp(TCHAR_TO_UTF8(*itr.Key) ,"description") == 0)
+                metadata.description = itr.Value->AsString();
+            if (std::strcmp(TCHAR_TO_UTF8(*itr.Key) ,"image") == 0)
+                metadata.image = itr.Value->AsString();
+            if (std::strcmp(TCHAR_TO_UTF8(*itr.Key) ,"external_url") == 0)
+                metadata.external_url = itr.Value->AsString();
+            if (std::strcmp(TCHAR_TO_UTF8(*itr.Key) ,"external_url_mimetype") == 0)
+                metadata.external_url_mimetype = itr.Value->AsString();
+            if (std::strcmp(TCHAR_TO_UTF8(*itr.Key) ,"animation_url") == 0)
+                metadata.animation_url = itr.Value->AsString();
+            if (std::strcmp(TCHAR_TO_UTF8(*itr.Key) ,"animation_url_mimetype") == 0)
+                metadata.animation_url_mimetype = itr.Value->AsString();
+
+            if (std::strcmp(TCHAR_TO_UTF8(*itr.Key) ,"properties") == 0) {
+                const FJsonObject & json_properties = *(itr.Value->AsObject());
+
+                for (auto itrr : json_properties.Values) {
+                    if(itrr.Value->Type == EJson::String)
+                        metadata.properties[itrr.Key] = itr.Value->AsString();
+                    if(itrr.Value->Type == EJson::Number)
+                        metadata.properties[itr.Key] = FString::Printf(TEXT("%.*f"),0, itr.Value->AsNumber());
+                }
+            }
+        }
+
+    }
+    catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    int x = 0;
+}
+
---
+++ Source/AlgorandAPI/Public/Arc/Arc03.h
@@ -0,0 +1,36 @@
+//
+// Created by Bruno on 6/4/2023.
+//
+
+#ifndef CPP_ARC03_H
+#define CPP_ARC03_H
+
+#include "ArcBase.h"
+#include <map>
+
+typedef struct {
+    FString standard;
+    FString name;
+    uint64_t decimals;
+    FString  description;
+    FString  image;
+    FString  image_mimetype;
+    FString  external_url;
+    FString  external_url_mimetype;
+    FString  animation_url;
+    FString animation_url_mimetype;
+    TMap<FString, FString> properties;
+} arc03_metadata;
+
+class Arc03 : public ArcBase{
+public:
+    Arc03() : ArcBase() {}
+    Arc03(uint64_t asset_id);
+
+    bool IsVerify();
+    void from_asset_url(std::string& url);
+    arc03_metadata metadata;
+};
+
+
+#endif //CPP_ARC03_H
